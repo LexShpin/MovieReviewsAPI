cd7185e0c856711a656aa47b0da51f1b16558112
lexshpin <lexshpin@gmail.com>
2024-02-10 12:19:07 -0500

    Initial setup

---
+++ src/test/java/com/lexshpin/MovieReviews/MovieReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lexshpin.MovieReviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
